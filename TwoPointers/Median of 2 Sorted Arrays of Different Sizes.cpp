6858c0c029f10d80e6cc30b3634df31c57d4d110
DSA <kangantiakash@gmail.com>
2025-05-22 17:53:07 +0530

    TwoPointers

--- TwoPointers/Median of 2 Sorted Arrays of Different Sizes.cpp
+++ TwoPointers/Median of 2 Sorted Arrays of Different Sizes.cpp
@@ -3,30 +3,46 @@
 using namespace std;
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int n1 = nums1.size();
-    int n2 = nums2.size();
-    vector<int> arr(n1 + n2);
-    int i = n1 - 1, j = n2 - 1, k = n1 + n2 - 1;
-
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] >= nums2[j]) {
-            arr[k--] = nums1[i--];
-        } else {
-            arr[k--] = nums2[j--];
+    int n1=nums1.size();
+    int n2=nums2.size();
+    vector<int> arr(n1+n2);
+    int i=n1-1,j=n2-1,k=n1+n2-1;
+    while(i>=0 && j>=0)
+    {
+        if(nums1[i]>=nums2[j])
+        {
+            arr[k] = nums1[i];
+            k--;
+            i--;
+        }
+        else{
+            arr[k] = nums2[j];
+            k--;
+            j--;
         }
     }
-    while (j >= 0) {
-        arr[k--] = nums2[j--];
+    while(j>=0)
+    {
+        arr[k] = nums2[j];
+        k--;
+        j--;
     }
-    while (i >= 0) {
-        arr[k--] = nums1[i--];
+    while(i>=0)
+    {
+        arr[k] = nums1[i];
+        k--;
+        i--;
     }
-
     int n = arr.size();
-    if (n % 2 == 0) {
-        return (arr[n / 2] + arr[n / 2 - 1]) / 2.0;
-    } else {
-        return double(arr[n / 2]);
+    if(arr.size()%2==0)
+    {
+        double x = arr[n/2]+arr[(n/2)-1];
+        x = x/2;
+        return x;
+    }
+    else{
+        double x = double(arr[arr.size()/2]);
+        return x;
     }
 }
 
